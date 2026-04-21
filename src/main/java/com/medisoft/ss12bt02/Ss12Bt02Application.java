b4d98cce568d3d7a247eba4c46d3f60ab6c4b579
ninhpoh <260806tan@example.com>
2026-04-21 22:10:27 +0700

    first commit

---
+++ src/main/java/com/medisoft/ss12bt02/Ss12Bt02Application.java
@@ -0,0 +1,13 @@
+package com.medisoft.ss12bt02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss12Bt02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss12Bt02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/medisoft/ss12bt02/Ss12Bt02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisoft.ss12bt02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss12Bt02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
